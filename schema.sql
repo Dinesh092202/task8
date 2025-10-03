eff4d501f0be6b7b1ea4ed6e1d521bcb6cb0c3d5
Dinesh092202 <dineshlodhi608@gmail.com>
2025-10-03 13:41:10 +0530

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Employees (
+    EmpID INT PRIMARY KEY,
+    EmpName VARCHAR(50),
+    Salary DECIMAL(10,2),
+    Department VARCHAR(50)
+);
+
+CREATE TABLE Departments (
+    DeptID INT PRIMARY KEY,
+    DeptName VARCHAR(50)
+);
